e76c2069c5e1a5646322e9ec1f0b9557e93e081d
司晓翰 <a361162743@hotmail.com>
2016-10-26 00:25:34 +0800

    login success

--- app/controllers/spiders_controller.rb
+++ app/controllers/spiders_controller.rb
@@ -1,5 +1,14 @@
 class SpidersController < ApplicationController
   def index
+    agent = Mechanize.new
+    login_page = agent.get('https://www.tumblr.com/login')
+    @loggedin_page = login_page.form_with(:id => 'signup_form') do |form|
+      @form = form.fields
+      email_field = form.field_with(:name => 'user[email]')
+      email_field.value = 'a361162743@hotmail.com'
+      password_field = form.field_with(:name => 'user[password]')
+      password_field.value = '88siyang'
+    end.submit
 
   end
 end
