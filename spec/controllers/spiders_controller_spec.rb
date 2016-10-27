20b06624ad5c977658afdad7fbe78de9bdf50bff
司晓翰 <a361162743@hotmail.com>
2016-10-27 16:50:28 +0800

    test rspec failure for jenkins

--- spec/controllers/spiders_controller_spec.rb
+++ spec/controllers/spiders_controller_spec.rb
@@ -5,7 +5,7 @@ describe SpidersController do
   describe 'index' do
     it 'index' do
       get 'index'
-      expect(response).to render_template :index
+      expect(response).to_not render_template :index
     end
   end
 end
